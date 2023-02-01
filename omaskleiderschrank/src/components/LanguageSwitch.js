9a3da05b6e17bb1a0dc3949c6b885484e00c4c75
HegedusAlexandra <hexapastry@gmail.com>
2023-02-01 15:25:03 +0100

    finished,before review

--- omaskleiderschrank/src/components/LanguageSwitch.js
+++ omaskleiderschrank/src/components/LanguageSwitch.js
@@ -4,8 +4,8 @@ import "./components.css";
 export default function LanguageSwitch({ handleClick }) {
   return (
     <div className="language">
-      <button onClick={(event) => handleClick("de")}>DE</button>/
-      <button onClick={(event) => handleClick("hu")}>HU</button>/
+      <button onClick={(event) => handleClick("de")}>DE/</button>
+      <button onClick={(event) => handleClick("hu")}>HU/</button>
       <button onClick={(event) => handleClick("en")}>EN</button>
     </div>
   );
